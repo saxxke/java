3cebebdf424a9c60361cf714215bcacf9c7488e6
saxxke <kingshukadhikari2021@gmail.com>
2024-09-13 23:22:29 +0530

    ok

---
+++ STRING/strinput.java
@@ -0,0 +1,10 @@
+public class strinput {
+    public static void main(String[] args) {
+        String name = "Kunal Kushwaha";
+        System.out.println(name);
+        String a = "Kunal";
+        System.out.println(a);
+        a = "Kushwaha";
+        System.out.println(a);
+    }
+}
