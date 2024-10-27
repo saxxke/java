f239ce5444430c28415f457aca8c81736d12b927
saxxke <kingshukadhikari2021@gmail.com>
2024-10-27 21:48:30 +0530

    ok

--- STRING/prettyprint.java
+++ STRING/prettyprint.java
@@ -3,6 +3,6 @@ public class prettyprint {
         float a = 453.1274f;
         System.out.printf("Formatted number is %.2f", a );
         System.out.printf("Pie: %.3f", Math.PI);
-        System.out.printf("Hello my name is %s and I am %s", "Kunal", "Cool");
+        System.out.printf("Hello my name is %s and I am %s", "Kingshuk", "Cool");
     }
 }
